945223c839ed11757bab2e6705fde8d66bc1d248
nudzik <jaroslavt@gmail.com>
2024-03-19 11:10:12 +0000

    Changes to be committed: 	modified:   keyboards/crkbd/keymaps/default/config.h 	modified:   keyboards/crkbd/rev1/info.json

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -47,3 +47,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
+
+/* Double tap the side button to enter bootloader */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP13
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
